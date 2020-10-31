4a750858f5a8ac31700871d2e27b2560bbccc762
cmcaudill <73039552+cmcaudill@users.noreply.github.com>
2020-10-30 20:07:11 -0400

    Log in process plus pet entry

---
+++ PetminderApp/PetminderApp/PetminderApp/SignIn.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PetminderApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SignIn : ContentPage
+    {
+        public SignIn()
+        {
+            InitializeComponent();
+
+            signIn.Clicked += SignIn_Clicked;
+        }
+
+        private async void SignIn_Clicked(object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new HomeScreen());
+        }
+
+        private async void GoToSignUp_Tapped(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SignUp());
+        }
+    }
+}
