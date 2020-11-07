6566200920be75630d3f7bce5627a65996707911
engelejm <60905631+engelejm@users.noreply.github.com>
2020-11-07 16:03:33 -0500

    Update AddPet.xaml.cs

--- PetminderApp/PetminderApp/PetminderApp/Views/AddPet.xaml.cs
+++ PetminderApp/PetminderApp/PetminderApp/Views/AddPet.xaml.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using Plugin.FilePicker;
+using Plugin.FilePicker.Abstractions;
 namespace PetminderApp
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
@@ -17,15 +19,31 @@ namespace PetminderApp
             InitializeComponent();
 
             submitPet.Clicked += SubmitPet_Clicked;
+            uploadPet.Clicked += UploadPet_Clicked;
 
-            
         }
 
         private async void SubmitPet_Clicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new HomeScreen());
         }
+        private async void UploadPet_Clicked(object sender, EventArgs e)
+        {
 
+            try
+            {
+
+                FileData filedata = await CrossFilePicker.Current.PickFile();
+                // the dataarray of the file will be found in filedata.DataArray 
+                // file name will be found in filedata.FileName;
+                //etc etc.
+
+            }
+            catch (Exception ex)
+            {
+            }
+
+        }
         private async void AddAnotherPet_Tapped(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new AddPet());
