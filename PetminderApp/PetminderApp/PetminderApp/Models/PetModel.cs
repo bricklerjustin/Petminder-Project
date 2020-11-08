916e0bfe4e3d618c64c034b674510fe4d584f4b7
bricklerjustin <bricklerjustin@gmail.com>
2020-11-08 12:14:53 -0500

    Pet model definition

---
+++ PetminderApp/PetminderApp/PetminderApp/Models/PetModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetminderApp.Models
+{
+    public class PetModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Weight { get; set; }
+        public int? Age { get; set; }
+        public string Type { get; set; }
+        public string Breed { get; set; }
+        public Guid AccountId { get; set; }
+    }
+}
