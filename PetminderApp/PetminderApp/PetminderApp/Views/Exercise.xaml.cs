315c64599c52d2101190fcc6c4b9f860e563d5c4
engelejm <60905631+engelejm@users.noreply.github.com>
2020-11-21 16:07:46 -0500

    Update Exercise.xaml.cs

--- PetminderApp/PetminderApp/PetminderApp/Views/Exercise.xaml.cs
+++ PetminderApp/PetminderApp/PetminderApp/Views/Exercise.xaml.cs
@@ -1,14 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Diagnostics;
-using System.Threading;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using Xamarin.Essentials;
-using Plugin.Geolocator;
 
 namespace PetminderApp.Views
 {
